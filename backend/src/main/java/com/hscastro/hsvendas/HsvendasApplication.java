c8400c1e37b9958dff9423eec84c667717752041
Antonio Halyson <halisonsc5@gmail.com>
2021-05-05 06:15:51 -0300

    project created

---
+++ backend/src/main/java/com/hscastro/hsvendas/HsvendasApplication.java
@@ -0,0 +1,13 @@
+package com.hscastro.hsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hscastro/hsvendas/HsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hscastro.hsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
