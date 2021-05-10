8e717b7a681666144ee62cae836b699f6350552e
Antonio Halyson <halisonsc5@gmail.com>
2021-05-09 22:51:58 -0300

    Pagination

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -16,11 +16,11 @@ const DonutChart = () => {
     useEffect(() => {       
         axios.get(`${BASE_URL}/sales/amount-by-seller`)
             .then(response => {
-                const data = response.data as SaleSum[];                
+                const data = response.data as SaleSum[]; 
                 const myLabels = data.map(x => x.sellerName);
                 const mySeries = data.map(x => x.sum);
 
-                setChartData({labels: myLabels, series: mySeries});
+                setChartData({labels: myLabels, series: mySeries})
 
             });
     }, []);
