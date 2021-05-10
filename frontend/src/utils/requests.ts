178ce7df642b74d23f309eb97d83b766a44a575d
Antonio Halyson <halisonsc5@gmail.com>
2021-05-09 23:03:39 -0300

    Env var

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,2 +1,4 @@
-export const BASE_URL='http://localhost:8080'; 
+export const BASE_URL= process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080'; 
+//export const BASE_URL='http://localhost:8080'; 
+
 //export const BASE_URL='https://sd3-halyson.herokuapp.com';
